071e2ebbbae5ff72d8740857511a26a642082d77
Jarod Paulson <jarodp@students.cs.ubc.ca>
2025-02-12 10:14:17 -0800

    Added specification for VolumeTimeScalar

--- src/main/model/VolumeTimeScalar.java
+++ src/main/model/VolumeTimeScalar.java
@@ -1,5 +1,38 @@
 package model;
 
-public class VolumeTimeScalar {
-    
+public class VolumeTimeScalar extends ScalarTime {
+
+    public VolumeTimeScalar(int tidalVolume, int respRate, int compliance, float resistance) {
+        super(tidalVolume, respRate, compliance, resistance);
+        // TODO Auto-generated constructor stub
+    }
+
+    // EFFECTS: returns the tidal volume of this
+    @Override
+    protected void calculateAmplitude() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateAmplitude'");
+    }
+
+    // EFFECTS: returns 0
+    @Override
+    protected void calculatePhaseShift() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculatePhaseShift'");
+    }
+
+    // EFFECTS: returns 0
+    @Override
+    protected void calculateVertShift() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateVertStretch'");
+    }
+
+    // EFFECTS: returns 2pi / total breath cycle time
+    @Override
+    protected void calculateConversionFactor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateHorStretch'");
+    }
+
 }
