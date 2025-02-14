6a17fdded7d503950b9c6bc278b929d0ffdf81b4
Jarod Paulson <jarodp@students.cs.ubc.ca>
2025-02-14 07:44:23 -0800

    Added specification for PressureTimeScalar

--- src/main/model/PressureTimeScalar.java
+++ src/main/model/PressureTimeScalar.java
@@ -1,6 +1,41 @@
 package model;
 
-public class PressureTimeScalar {
+public class PressureTimeScalar extends ScalarTime {
+
+    protected PressureTimeScalar(int tidalVolume, int respRate, int compliance, float resistance) {
+            super(tidalVolume, respRate, compliance, resistance);
+            //TODO Auto-generated constructor stub
+        }
+    
+        @Override
+    public String getUnits() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUnits'");
+    }
+
+    @Override
+    public String getScalarName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getScalarName'");
+    }
+
+    @Override
+    public float calculateAmplitude() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateAmplitude'");
+    }
+
+    @Override
+    protected float calculatePhaseShift() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculatePhaseShift'");
+    }
+
+    @Override
+    protected float calculateVertShift() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateVertShift'");
+    }
 
     
 }
